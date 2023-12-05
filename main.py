336e74a678d398944204cdcb553b3d636b106868
Nikhil Mandge <Nikhil.Mandge@qorvo.com>
2023-12-05 12:49:16 -0500

    content type changed to text/plain

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from flask import Flask,Response,request,json
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
+def notification_url():
+    validationToken = request.args.get('validationToken')
+    data = validationToken
+    #Call yout appian REST API
+    #     
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='text/plain'
+    ) 
+    return response
+
+@app.route('/',methods=['POST'])
 def notification_url():
     validationToken = request.args.get('validationToken')
     data = validationToken
